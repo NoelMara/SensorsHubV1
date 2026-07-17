e50145100ca55b1d9b03323fbda0b03490ca4191
Noel Mara <pro.noelmara13@gmail.com>
2026-07-17 11:44:06 +0800

    Add overdue status for activities past due date

--- resources/views/user/classes/show.blade.php
+++ resources/views/user/classes/show.blade.php
@@ -87,6 +87,8 @@
                                 @else
                                     <span class="px-2 py-1 text-xs rounded-full bg-blue-100 text-blue-800">Submitted</span>
                                 @endif
+                           @elseif($activity->due_date && now()->isAfter($activity->due_date))
+                                <span class="px-2 py-1 text-xs rounded-full bg-red-100 text-red-800">Overdue</span>
                             @else
                                 <span class="px-2 py-1 text-xs rounded-full bg-yellow-100 text-yellow-800">Pending</span>
                             @endif
