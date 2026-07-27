7fd4ca56d7ad5fdb1f426956d4da636b84efb1e1
Noel Mara <pro.noelmara13@gmail.com>
2026-07-27 21:27:31 +0800

    Clean file size validation with inline error

--- resources/views/instructor/classes/modules/create.blade.php
+++ resources/views/instructor/classes/modules/create.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="p-6">
-            <form method="POST" action="{{ route('instructor.classes.modules.store', $class) }}" enctype="multipart/form-data" onsubmit="const f = document.getElementById('file').files[0]; if(f && f.size > 10485760) { document.getElementById('fileSizeError').classList.remove('hidden'); event.submitter.disabled = false; return false; }">
+            <form method="POST" action="{{ route('instructor.classes.modules.store', $class) }}" enctype="multipart/form-data" onsubmit="const f = document.getElementById('file').files[0]; if(f && f.size > 10485760) { document.getElementById('fileSizeError').classList.remove('hidden'); return false; }">
                 @csrf
 
                 <div class="space-y-5">
