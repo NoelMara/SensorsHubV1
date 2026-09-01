44552a57a53b1208cafb7d9c4ca807db2e46a07d
Noel Mara <pro.noelmara13@gmail.com>
2026-09-01 23:44:33 +0800

    Skip system table sequences in backup reset

--- app/Http/Controllers/Administrator/DashboardController.php
+++ app/Http/Controllers/Administrator/DashboardController.php
@@ -199,8 +199,10 @@ class DashboardController extends Controller
         // Reset sequences to avoid duplicate key errors on restore
         $output .= "\n-- Reset Sequences\n";
         $sequences = \DB::select("SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema = 'public'");
+        $skipSequences = ['migrations_id_seq', 'jobs_id_seq', 'failed_jobs_id_seq', 'cache_id_seq', 'sessions_id_seq'];
         foreach ($sequences as $sequence) {
             $seqName = $sequence->sequence_name;
+            if (in_array($seqName, $skipSequences)) continue;
             $tableName = preg_replace('/_id_seq$/', '', $seqName);
             $output .= "SELECT setval('{$seqName}', COALESCE((SELECT MAX(id) FROM {$tableName}), 1));\n";
         }
